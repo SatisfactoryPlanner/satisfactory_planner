44eefc4d4c3246ef5ea49451f12f5670b0466692
localcc <work@localcc.cc>
2023-01-14 16:09:12 +0100

    planner_lib: made byproduct recalculation

--- planner_lib/src/lib.rs
+++ planner_lib/src/lib.rs
@@ -12,7 +12,9 @@ mod tests {
     fn it_works() {
         let registry = Registry::new();
 
-        let recipe = registry.get_default_recipe("Fused Modular Frame").unwrap();
+        let recipe = registry
+            .get_default_recipe("Thermal Propulsion Rocket")
+            .unwrap();
         let graph = recipe_graph::generate_graph(recipe, 60f32, &registry);
         println!("{:?}", Dot::new(&graph));
     }
--- planner_lib/src/recipe_graph.rs
+++ planner_lib/src/recipe_graph.rs
@@ -1,6 +1,10 @@
 use std::{collections::HashMap, rc::Rc};
 
-use petgraph::{stable_graph::NodeIndex, Graph};
+use petgraph::{
+    stable_graph::NodeIndex,
+    visit::{Dfs, EdgeRef},
+    Graph,
+};
 use planner_registry::{
     items::{item::Item, recipe::Recipe},
     Registry,
@@ -29,6 +33,10 @@ fn add_recipe_to_graph(
     graph: &mut Graph<Rc<Item>, f32>,
 ) -> Vec<(&'static str, f32, NodeIndex)> {
     let mut next_round = Vec::new();
+
+    if recipe.output.item.raw {
+        return Vec::new();
+    }
 
     let per_minute_multiplier = 60f32 / recipe.manufacturing_duration;
     let crafted_per_minute = recipe.output.amount as f32 * per_minute_multiplier;
@@ -62,6 +70,45 @@ fn add_recipe_to_graph(
     next_round
 }
 
+fn add_recipe_tree_to_graph(
+    existing_nodes: &mut HashMap<&str, NodeIndex>,
+    graph: &mut Graph<Rc<Item>, f32>,
+    root_node: NodeIndex,
+    expected_output_per_minute: f32,
+    registry: &Registry,
+    recipe: &Recipe,
+) {
+    let mut current_round = add_recipe_to_graph(
+        existing_nodes,
+        root_node,
+        expected_output_per_minute,
+        recipe,
+        graph,
+    );
+    let mut next_round = Vec::new();
+
+    loop {
+        for (name, expected_output_per_minute, node) in current_round {
+            if let Some(recipe) = registry.get_default_recipe(name) {
+                next_round.extend(add_recipe_to_graph(
+                    existing_nodes,
+                    node,
+                    expected_output_per_minute,
+                    recipe,
+                    graph,
+                ));
+            }
+        }
+
+        if next_round.is_empty() {
+            break;
+        }
+
+        current_round = next_round;
+        next_round = Vec::new();
+    }
+}
+
 pub fn generate_graph(
     recipe: &Recipe,
     expected_output_per_minute: f32,
@@ -74,34 +121,55 @@ pub fn generate_graph(
     let root_node = graph.add_node(recipe.output.item.clone());
 
     let mut existing_nodes = HashMap::new();
-    let mut current_round = add_recipe_to_graph(
+
+    add_recipe_tree_to_graph(
         &mut existing_nodes,
+        &mut graph,
         root_node,
         expected_output_per_minute,
+        registry,
         recipe,
-        &mut graph,
     );
-    let mut next_round = Vec::new();
 
-    loop {
-        for (name, expected_output_per_minute, node) in current_round {
-            if let Some(recipe) = registry.get_default_recipe(name) {
-                next_round.extend(add_recipe_to_graph(
-                    &mut existing_nodes,
-                    node,
-                    expected_output_per_minute,
-                    recipe,
-                    &mut graph,
-                ));
-            }
+    // recalculating with account for byproducts
+    let mut dfs = Dfs::new(&graph, root_node);
+    while let Some(node) = dfs.next(&graph) {
+        let needs_recalculating = graph
+            .edges_directed(node, petgraph::Direction::Incoming)
+            .any(|e| *e.weight() > 0f32);
+
+        if !needs_recalculating {
+            continue;
         }
 
-        if next_round.is_empty() {
-            break;
+        let byproduct_only = graph
+            .edges_directed(node, petgraph::Direction::Incoming)
+            .all(|e| *e.weight() > 0f32);
+
+        if byproduct_only {
+            continue;
         }
 
-        current_round = next_round;
-        next_round = Vec::new();
+        let sum = graph
+            .edges_directed(node, petgraph::Direction::Incoming)
+            .map(|e| *e.weight())
+            .sum::<f32>();
+
+        let name = graph.node_weight(node).unwrap().name;
+
+        let outgoing_edges = graph
+            .edges_directed(node, petgraph::Direction::Outgoing)
+            .map(|e| e.id())
+            .collect::<Vec<_>>();
+
+        for outgoing_edge in outgoing_edges {
+            graph.remove_edge(outgoing_edge);
+        }
+
+        // todo: recipe override
+        if let Some(recipe) = registry.get_default_recipe(name) {
+            add_recipe_tree_to_graph(&mut existing_nodes, &mut graph, node, sum, registry, recipe);
+        }
     }
 
     graph
