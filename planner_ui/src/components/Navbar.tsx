1e910cde6975dbc3e4dd654f2d5590004c6e9861
Artem Dzhemesiuk <programming@zziger.me>
2023-05-09 15:25:17 +0200

    Yay

--- planner_ui/src/components/Navbar.tsx
+++ planner_ui/src/components/Navbar.tsx
@@ -1,10 +1,22 @@
 import {Box, Divider, Typography} from "@mui/joy";
+import {useEffect, useState} from "react";
 
 export default function Navbar() {
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        if (counter >= 5) {
+            document.body.classList.add('yay');
+        }
+    }, [counter])
   return (
     <nav>
       <Box sx={{ width: "100%" }}>
-        <Typography level="h4" sx={{ px: 2, py: 0.5 }}>Satisfactory Planner</Typography>
+        <Typography level="h4" sx={{ px: 2, py: 0.5, userSelect: 'none', cursor: 'pointer' }} onClick={() => {
+            setCounter(counter + 1);
+        }}>
+            Satisfactory Planner
+        </Typography>
         <Divider />
       </Box>
     </nav>
